41f9cb0fb2ede51bf1ce00bf9f2ef007512429b6
minkas-g-d <mina.dodunekova@gmail.com>
2021-01-11 01:22:10 +0200

    Change favicon and some metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	siteMetadata: {
-		title: `Novela by Narative`,
-		name: `Narative`,
+		title: `BAM adventures`,
+		name: `BAM adventures`,
 		siteUrl: `https://novela.narative.co`,
 		description: `This is my description that will be used in the meta tags and important for search results`,
 		hero: {
